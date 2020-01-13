16b5fb11681a5006801ec84e22c4ece69fdfd474
Lasse Arkko <lasse.arkko@gmail.com>
2020-01-13 16:37:27 +0200

    Implement logic for calling callback in component
    The callback is now continuously called when relevant (i.e. the callback exists and the canvas reference has been acquired).

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React, {
   useRef,
+  useEffect,
 } from 'react';
+
+function useCanvasCallback(canvasRef, callback) {
+  useEffect(() => {
+    /*Only relevant if canvas and callback exist,
+      otherwise there is nothing that can be done.*/
+    if (canvasRef.current && callback) {
+      /*Keeps track of the request ID, which we need to be
+        able to cancel an animation frame.*/
+      let requestID = null;
+      /*Wrapper that handles the management related to
+        calling the animation frames as well as calling
+        the callback itself*/
+      const step = () => {
+        callback(canvasRef.current);
+        requestID = window.requestAnimationFrame(step);
+      };
+      step();
+      /*Prevents step function from being called anymore*/
+      const cleanup = () => {
+        if (requestID) {
+          window.cancelAnimationFrame(requestID);
+        }
+      };
+      return cleanup;
+    }
+  }, [canvasRef, callback]);
+}
 
 export default function Canvas({
   /*A callback function provided to the canvas*/
@@ -10,6 +38,7 @@ export default function Canvas({
   ...otherProps
 }) {
   const canvasRef = useRef(null);
+  useCanvasCallback(canvasRef, callback);
   return (
     <canvas
       ref={canvasRef}
